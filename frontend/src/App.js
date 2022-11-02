9fba2c3307e9fec6cf1f0c81eb38cf3396bf00a1
Sachini Abeywardhana <sachisach983@gmail.com>
2022-11-02 14:41:36 +0530

    item and inventory

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,27 @@
 import './App.css';
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Inventry from './components/inventry_mgmt/Inventry';
+import UpdateInventry from './components/inventry_mgmt/UpdateInventry';
+import Item from './components/inventry_mgmt/Item';
+import UpdateItem from './components/inventry_mgmt/UpdateItem';
+import Order from './components/order_mgmt/Order';
+import UpdateOrder from './components/order_mgmt/UpdateOrder';
 
 function App() {
   return (
     <div className="App">
-     
+      <Router>
+        <Routes>
+          <Route path='/inventry' element={<Inventry />}></Route>
+          <Route path='/updateinventry' element={<UpdateInventry />}></Route>
+          <Route path='/item' element={<Item />}></Route>
+          <Route path='/updateitem' element={<UpdateItem />}></Route>
+          <Route path='/order' element={<Order />}></Route>
+          <Route path='/updateorder' element={<UpdateOrder />}></Route>
+        </Routes>
+      </Router>
+
     </div>
   );
 }
---
+++ frontend/src/components/inventry_mgmt/Inventry.js
@@ -0,0 +1,75 @@
+import React from "react"
+import { TextField, Button, Stack, Paper, TextareaAutosize } from "@mui/material";
+import { textAlign } from "@mui/system";
+
+function Inventry() {
+    return (
+        <diV>
+
+            <Stack direction={'row'} flex={1} sx={{ marginTop: 10 }}>
+                <Stack direction={'col'} flex={1}></Stack>
+                <Stack direction={'col'}>
+                    <Paper variant="elevation" sx={{ padding: '30px', marginTop: '30px', backgroundColor: 'whitesmoke' }} >
+                        <Stack spacing={2} direction={'row'}>
+                            <h2 style={{ fontFamily: 'sans-serif', color: 'black' }}>ADD INVENTRY DETAILS</h2>
+                        </Stack><br />
+                        <div>
+                            <form class="form-card" >
+
+
+                                <div class="row justify-content-between text-left" >
+                                    <Stack spacing={2} direction={'row'}>
+                                        <TextField sx={{ backgroundColor: 'white' }} label="Inventory ID" size="small" id="inventory_id" name="inventory_id" placeholder="Inventry ID">
+                                        </TextField>
+                                        <TextField  sx={{ backgroundColor: 'white' }} label="Inventory_Type" size="small" id="inventory_type" name="inventory_type" placeholder="Inventory Type" onblur="validate(2)" >
+                                        </TextField>
+                                    </Stack>
+                                </div><br />
+                                <div class="row justify-content-between text-left">
+                                    <Stack spacing={2} direction={'row'}>
+                                        <TextField sx={{ backgroundColor: 'white' }} label="Inventory Location" size="small" id="inventory_location" name="inventory_location" placeholder="Inventory Location" onblur="validate(3)">
+                                        </TextField>
+                                        <TextField  sx={{ backgroundColor: 'white' }} label="Inventory_Name" size="small" id="inventory_name" name="inventory_name" placeholder="Inventory Name" onblur="validate(4)" >
+                                        </TextField>
+                                    </Stack>
+                                </div><br />
+                                <div class="row justify-content-between text-left">
+                                    <Stack spacing={2} s>
+
+                                        <TextareaAutosize sx={{ backgroundColor: 'white' }} style={{ height: 60 }} size="small" id="inventory_description" name="inventory_description" placeholder="Description" onblur="validate(5)" >
+                                        </TextareaAutosize>
+                                    </Stack>
+                                </div><br />
+
+
+
+                                <div class="row justify-content-between text-left">
+                                    <Stack spacing={2} direction={'row'}>
+                                        <Button type="submit" size="medium" variant="contained" color="error" onClick={() => window.location.href = ""} sx={{ width: '49%' }}> Cancel </Button>
+                                        <Button type="submit" size="medium" variant="contained" color="primary" sx={{ width: '49%' }}>ADD INVENTRY</Button>
+                                    </Stack>
+
+                                </div>
+
+
+                            </form>
+
+
+
+                        </div>
+                    </Paper>
+                </Stack>
+                <Stack direction={'col'} flex={1}></Stack>
+            </Stack>
+            <Stack spacing={1} >
+
+                <Button type="submit" size="medium" variant="contained" color="success" sx={{ width: '10%', marginLeft: 120, marginTop: 5 }}>VIEW INVENTRY</Button>
+            </Stack>
+
+
+        </diV>
+
+    )
+}
+
+export default Inventry
