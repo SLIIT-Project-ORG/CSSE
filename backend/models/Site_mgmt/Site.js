04056573d2694cf0367b99eacabb5f7a271ffb28
nethunipuna <nethunipuna7@gmail.com>
2022-11-03 14:40:00 +0530

    site management

---
+++ backend/models/Site_mgmt/Site.js
@@ -0,0 +1,31 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const siteSchema = new Schema({
+
+    sitename:{
+        type:String,
+        required:true
+    },
+    site_location:{
+        type:String,
+        required:true
+    },
+    siteManager:{
+        type:String,
+        required:true
+    },
+    con_type:{
+        type:String,
+        required:true
+    },
+    emp_num:{
+        type:String,
+        required:true
+    }
+   
+
+})
+
+const site = mongoose.model("sites",siteSchema);
+module.exports = site;
